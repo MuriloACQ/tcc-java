03648d46b857b955b2aae1dd60e0a04f59f21e0d
MuriloACQ <macquadros@gmail.com>
2014-04-25 18:00:02 -0300

    apply look and feel like windows

--- tcc-java/src/view/Authentication.java
+++ tcc-java/src/view/Authentication.java
@@ -3,24 +3,32 @@ package view;
 import javax.swing.JOptionPane;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+import javax.swing.UIManager;
 
 import core.System;
-
 import vo.Auth;
 
 public class Authentication {
 
 	public static Auth getAuthPanel() {
+		try {
+			UIManager
+					.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+		} catch (Exception e) {
+		}
+
 		JTextField username = new JTextField();
 		JTextField password = new JPasswordField();
 		Object[] message = { "Usuário:", username, "Senha:", password };
 
-		int option = JOptionPane.showConfirmDialog(null, message,"Autenticador", JOptionPane.OK_CANCEL_OPTION);
+		int option = JOptionPane.showConfirmDialog(null, message,
+				"Autenticador", JOptionPane.OK_CANCEL_OPTION);
 		if (option == JOptionPane.OK_OPTION) {
 			return new Auth(username.getText(), password.getText());
-		} 
-		int response = Messager.getConfirmPanel("Atenção", "Você dejesa abortar o processo?");
-		if(response == JOptionPane.YES_OPTION){
+		}
+		int response = Messager.getConfirmPanel("Atenção",
+				"Você dejesa abortar o processo?");
+		if (response == JOptionPane.YES_OPTION) {
 			System.ABORT = true;
 		}
 		return null;
